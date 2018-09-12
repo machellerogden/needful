c3cc74fbaf5d5e73592a6542ae5ef304b3becd3f
Mac Heller-Ogden <mac@machellerogden.com>
2018-09-12 07:20:12 -0500

    better

--- index.js
+++ index.js
@@ -12,43 +12,9 @@ exports.pipe = (...fns) => (...args) => compose(...fns.reverse())(...args);
 
 exports.isObject = v => v != null
     && typeof v === 'object'
-    && !Array.isArray(v);
+    && v.constructor
+    && v.constructor.name === 'Object';
 
-exports.isPlainObject = v => v != null
-    && typeof v === 'object'
-    && !Array.isArray(v)
-    && typeof v !== 'function'
-    && !util.types.isProxy(v)
-    && !util.types.isAnyArrayBuffer(v)
-    && !util.types.isArgumentsObject(v)
-    && !util.types.isAsyncFunction(v)
-    && !util.types.isBigInt64Array(v)
-    && !util.types.isBigUint64Array(v)
-    && !util.types.isBooleanObject(v)
-    && !util.types.isDataView(v)
-    && !util.types.isExternal(v)
-    && !util.types.isFloat64Array(v)
-    && !util.types.isGeneratorFunction(v)
-    && !util.types.isGeneratorObject(v)
-    && !util.types.isInt8Array(v)
-    && !util.types.isInt16Array(v)
-    && !util.types.isInt32Array(v)
-    && !util.types.isMap(v)
-    && !util.types.isMapIterator(v)
-    && !util.types.isModuleNamespaceObject(v)
-    && !util.types.isNativeError(v)
-    && !util.types.isNumberObject(v)
-    && !util.types.isPromise(v)
-    && !util.types.isProxy(v)
-    && !util.types.isRegExp(v)
-    && !util.types.isSet(v)
-    && !util.types.isSetIterator(v)
-    && !util.types.isStringObject(v)
-    && !util.types.isSymbolObject(v)
-    && !util.types.isTypedArray(v)
-    && !util.types.isUint8ClampedArray(v)
-    && !util.types.isUint16Array(v)
-    && !util.types.isUint32Array(v)
-    && !util.types.isWeakMap(v)
-    && !util.types.isWeakSet(v)
-    && !util.types.isWebAssemblyCompiledModule(v);
+exports.isPlainObject = v => exports.isObject(v)
+    && !util.types.isArgumentsObject(v) // Object.prototype.toString.call(v) === '[object Arguments]' 
+    && !util.types.isProxy(v);
